22caadd65ad56081dba434850d979c655b98bf79
Linekodi <linekodi@gmail.com>
2023-02-22 22:19:01 -0300

    Add files via upload

--- TioJarvisBlogArquivos/plugin.video.realhub/resources/lib/scraper.py
+++ TioJarvisBlogArquivos/plugin.video.realhub/resources/lib/scraper.py
@@ -1,40 +1,38 @@
 import requests
-from bs4 import BeautifulSoup
+import re
 
-# URL base do site que estamos raspando
-BASE_URL = 'https://warezcdn.com'
+# URLs das páginas de mídia
+MOVIES_URL = 'https://warezcdn.com/listing.php?type=movies'
+SERIES_URL = 'https://warezcdn.com/listing.php?type=series'
+ANIMES_URL = 'https://warezcdn.com/listing.php?type=animes'
 
-# Cria uma função para fazer uma requisição HTTP e retornar o HTML da página
+# Expressões regulares para extrair informações do HTML
+TITLE_REGEX = r'<div class="mvic-desc">\s*<a href=".+?">\s*(.+?)\s*</a>\s*</div>'
+IMAGE_REGEX = r'<div class="mvic-thumb">\s*<a href=".+?">\s*<img src="(.+?)"'
+URL_REGEX = r'<div class="mvic-desc">\s*<a href="(.+?)">\s*.+?\s*</a>\s*</div>'
+
+
 def get_url(url):
+    """
+    Realiza uma solicitação GET para uma URL e retorna o conteúdo da resposta.
+    """
     response = requests.get(url)
-    return response.text
+    return response.content.decode('utf-8')
 
-# Cria uma função para extrair informações de mídia de uma página HTML
+
 def extract_media_info(html):
-    soup = BeautifulSoup(html, 'html.parser')
+    """
+    Extrai informações de mídia (título, imagem e URL) do HTML da página.
+    """
+    titles = re.findall(TITLE_REGEX, html)
+    images = re.findall(IMAGE_REGEX, html)
+    urls = re.findall(URL_REGEX, html)
+
     media = []
-    for item in soup.select('div.card'):
-        title = item.select_one('h5.card-title a').text.strip()
-        link = item.select_one('h5.card-title a')['href']
-        image = item.select_one('img.card-img')['src']
-        type = get_media_type(link)
-        media.append({'title': title, 'link': link, 'image': image, 'type': type})
+    for i in range(len(titles)):
+        media.append({
+            'title': titles[i],
+            'image': images[i],
+            'url': urls[i]
+        })
     return media
-
-# Cria uma função para obter o tipo de mídia com base na URL do link
-def get_media_type(link):
-    if '/listing.php?type=movies' in link:
-        return 'movie'
-    elif '/listing.php?type=series' in link:
-        return 'series'
-    elif '/listing.php?type=animes' in link:
-        return 'anime'
-    else:
-        return 'unknown'
-
-# Cria uma função para obter a URL de reprodução da mídia
-def get_media_url(link):
-    html = get_url(BASE_URL + link)
-    soup = BeautifulSoup(html, 'html.parser')
-    url = soup.select_one('div#player iframe')['src']
-    return url
